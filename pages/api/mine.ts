a0508d3c06019551921db9059f82d370d69ad69c
TarpCityBitch <travistarpley0@gmail.com>
2023-01-24 16:29:42 -0800

    Simple UI , Set total supply

--- pages/api/mine.ts
+++ pages/api/mine.ts
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { ethers } from 'ethers';
+import { BigNumber, ethers } from 'ethers';
 import type { NextApiRequest, NextApiResponse } from 'next'
 import abi from '../../artifacts/abi/AiUp.json'; 
 type Data = {
@@ -11,30 +11,48 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
-    const provider = new ethers.providers.InfuraProvider('mainnet', process.env.INFURA_API_KEY)
-    // grab message and signature from body
-    const {msg, sig} = req.body; 
-    const publicAddr = ethers.utils.verifyMessage(msg, sig);
-    const nftContract = new ethers.Contract('', abi.abi, provider);
-    const tokenId = await nftContract.totalSupply();
-    const ownerAddr = await nftContract.ownerOf(tokenId);
-    
-    if(ownerAddr === publicAddr) { //check if user owns the minted nft 
-        // TODO 
-        // Sending Request to another API is not ideal 
-        // Defeats the purpose of edge function 
-        // Set up API close to private API 
-        const req = await fetch('ip/generateImage', {
-            method:"POST", 
-            headers: {
-                'Content-Type' : 'application/json' 
-            },
-            body: JSON.stringify({tokenId})
-        }).then(r => r.json()); 
-        const {image} = req; 
-        res.status(200).json({image, error: null})
-    } else {
-        res.status(200).json({error: "signature didn't match"})
+    try {
+        const devNetwork = {
+            name: "hardhat", 
+            chainId: parseInt(process.env.NEXT_PUBLIC_CHAIN_ID as string)
+        }
+        let provider; 
+        if(process.env.DEV) {
+            provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545/", devNetwork)
+        } else {
+            provider = new ethers.providers.InfuraProvider('mainnet', process.env.INFURA_API_KEY)
+        }
+     
+        // grab message and signature from body
+        const {msg, sig} = req.body; 
+        const publicAddr = ethers.utils.verifyMessage(msg, sig);
+        const nftContract = new ethers.Contract('0x5fbdb2315678afecb367f032d93f642f64180aa3', abi.abi, provider);
+        const rawTokenId = await nftContract.totalSupply();
+        const tokenId = rawTokenId.toString(); 
+        const parseTokenId = parseInt(tokenId); 
+        const ownerAddr = await nftContract.ownerOf(parseTokenId - 1); // Minted Tokens start from 0
+        console.log(ownerAddr, publicAddr); 
+        if(ownerAddr === publicAddr) { //check if user owns the minted nft 
+            
+            // Sending Request to another API is not ideal 
+            // Defeats the purpose of edge function 
+            // TODO 
+            // Set up API close to private API 
+            const param = 'subway on the moon'; 
+            const resp = await fetch('http://localhost:8000/generateImage', {
+                method:"POST", 
+                headers: {
+                    'Content-Type' : 'application/json' 
+                },
+                body: JSON.stringify({tokenId, param})
+            }).then(r => r.json()) 
+            
+            // const {image} = req; 
+            res.status(200).json({image: resp.image, error: null})
+        } else {
+            res.status(200).json({error: "signature didn't match"})
+        }
+    } catch(e: any) {
+        res.status(200).json({error:e.toString()})
     }
-    
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,23 @@ import { Inter } from '@next/font/google'
 import useWeb3Provider from '@/libs/web3'
 import { ethers } from 'ethers'
 import abi from '../artifacts/abi/AiUp.json';
+import { useEffect, useState } from 'react';
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const { connect, web3Provider } = useWeb3Provider();
+  const { connect, web3Provider, address } = useWeb3Provider();
+  const [totalSupply, setTotalSupply] = useState(''); 
 
+  useEffect(() => {
+    if(web3Provider && !totalSupply) {
+      const nftContract = new ethers.Contract('0x5fbdb2315678afecb367f032d93f642f64180aa3', abi.abi, web3Provider);
+      nftContract.totalSupply().then((raw: any) => {
+        const supply = raw.toString(); 
+        setTotalSupply(supply); 
+      })
+    }
+  }, [totalSupply, web3Provider])
+  
   const ConnectWallet = () => {
     connect();
   }
@@ -15,31 +27,71 @@ export default function Home() {
   const Mint = async () => {
     if (!web3Provider)
       return;
-    const signer = web3Provider.getSigner(); 
+    const properChainId = parseInt(process.env.NEXT_PUBLIC_CHAIN_ID as string);
+    console.log(web3Provider.network.chainId)
+    if (web3Provider.network.chainId !== properChainId) {
+      (window as any).ethereum.request({
+        method: "wallet_addEthereumChain",
+        params: [{
+          chainId: ethers.utils.hexlify(properChainId),
+          rpcUrls: ["http://127.0.0.1:8545"],
+          chainName: "Hardhat",
+          nativeCurrency: {
+            name: "Hardhat",
+            symbol: "GO",
+            decimals: 18
+          }
+        }]
+      });
+      return;
+    }
+
+    const signer = web3Provider.getSigner();
     // user signs message so api can verify the owner of Token is sending Request 
     // and to protect my API  
     // the API cost money :) 
-    const sig = await signer.signMessage('mining nft');
-    const nftContract = new ethers.Contract('', abi.abi, web3Provider);
-    await nftContract.mintToken({ value: ethers.utils.formatEther('0.1') })
+    try {
+      const sig = await signer.signMessage('mining nft');
+      const nftContract = new ethers.Contract('0x5fbdb2315678afecb367f032d93f642f64180aa3', abi.abi, web3Provider.getSigner());
+      const minting = await nftContract.mintToken({ value: ethers.utils.parseEther('0.1') })
+      const data = await minting.wait();
+      const supply = await nftContract.totalSupply();
+      setTotalSupply(supply.toString())
+      const uri = await nftContract.tokenURI(parseInt(supply.toString()));
+      console.log(uri);
+      // const resp = await fetch('api/mine', {
+      //   method: "POST",
+      //   headers: {
+      //     'Content-Type': 'application/json'
+      //   },
+      //   body: JSON.stringify({ msg: "mining nft", sig })
+      // }).then(r => r.json())
+    } catch (e) {
+      console.log(e);
+    }
 
-    const resp = await fetch('/api/mint', {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json'
-      }, 
-      body: JSON.stringify({msg: "mining nft", sig})
-    }).then(r => r.json())
-    const image = resp.image; 
   }
+
   return (
     <>
-      <div onClick={ConnectWallet}>
-        <h1>Connect</h1>
-
-      </div>
-      <div onClick={Mint}>
-        <h1>MINT</h1>
+      <div className='h-screen w-screen flex flex-col items-center py-24 bg-slate-800 text-white  font-poppins'>
+        <h1 className='mb-4 font-poppins text-4xl'>MINT A UNIQUE NFT WITH JUST A PROMPT. </h1>
+        <h1 className='text-2xl'>{totalSupply ? totalSupply : '?'}/100</h1>
+        <div>
+          <div>
+            <div className='mt-24 w-72 h-80 flex justify-center items-center border-4 border-slate-500 my-4 rounded-lg'>
+              <h1 className='font-poppins text-8xl'>?</h1>
+            </div>
+            <div className='border w-full py-2 px-2 rounded-lg'>
+              <input type="text" placeholder='Enter a prompt' className='border-none bg-slate-800 text-white focus:outline-0 w-full' />
+            </div>
+            <div className='flex justify-center'>
+              <div className='w-full py-2 flex justify-center border text-xl font-poppins rounded-lg my-2 cursor-pointer' onClick={address ? Mint : ConnectWallet}>
+                <h1>{address ? "MINT" : "CONNECT"}</h1>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
